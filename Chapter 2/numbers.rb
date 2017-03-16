958cbbf45c1e44de445ae669ac84c11e700fe1d8
Adam Snow <adamjohnsnow@icloud.com>
2017-03-16 19:05:03 +0000

    Renamed folders to PDF Chapters rather than website

--- Chapter 2/numbers.rb
+++ Chapter 2/numbers.rb
@@ -10,4 +10,4 @@ require 'date'
 timePassed = (DateTime.now - Date.parse('1980-11-03')).to_f * 24 * 60 *60
  puts 'I am ' + timePassed.to_s + ' seconds old'
 
-puts 'You are ' + ((1246000000 / 60 /60 /24 /365).to_i).to_s + ' years old if you are 1246 million seconds old'
+puts 'You are ' + ((1160000000 / 60 /60 /24 /365).to_i).to_s + ' years old if you are 1160 million seconds old'
